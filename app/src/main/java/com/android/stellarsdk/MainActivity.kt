18a0df6e6a4da8d355f04ead0fb5437f3fe0d922
chattip.s <chattip.soontaku@ifyoucan.com>
2019-05-24 22:45:12 +0700

    Improved UI

--- app/src/main/java/com/android/stellarsdk/MainActivity.kt
+++ app/src/main/java/com/android/stellarsdk/MainActivity.kt
@@ -3,6 +3,7 @@ package com.android.stellarsdk
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.android.stellarsdk.api.ApiManager
 import com.android.stellarsdk.api.callback.AccountCallback
 import com.android.stellarsdk.api.callback.BaseCallback
@@ -17,6 +18,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         btn_generate_keypair.setOnClickListener {
             val keyPair = KeyPair.random()
             pair = keyPair
@@ -27,31 +29,47 @@ class MainActivity : AppCompatActivity() {
         }
 
         btn_get_test_network_lumens.setOnClickListener {
+            cst_result_friendbot.visibility = View.GONE
+            pb_one.visibility = View.VISIBLE
             ApiManager().getFriendBot(pair?.accountId, object : BaseCallback {
                 override fun onApiError(errorMessage: String?) {
+                    pb_one.visibility = View.GONE
                     tv_result_friend_bot.text = errorMessage
+                    cst_result_friendbot.background =
+                        ContextCompat.getDrawable(this@MainActivity, R.drawable.result_fail_background)
                     cst_result_friendbot.visibility = View.VISIBLE
                 }
 
                 override fun onApiSuccess(success: String?) {
+                    pb_one.visibility = View.GONE
                     tv_result_friend_bot.text = success
+                    cst_result_friendbot.background =
+                        ContextCompat.getDrawable(this@MainActivity, R.drawable.result_success_background)
                     cst_result_friendbot.visibility = View.VISIBLE
                 }
             })
         }
 
         btn_get_account.setOnClickListener {
-            tv_account_detail_id.text = "Balances for account " + pair?.accountId
+            cst_account_detail.visibility = View.GONE
+            pb_two.visibility = View.VISIBLE
+            tv_account_detail_id.text = "Balances for account \n" + pair?.accountId
             ApiManager().getAccount(pair?.accountId, object : AccountCallback {
                 override fun onApiError(errorMessage: String?) {
+                    pb_two.visibility = View.GONE
                     tv_account_detail.text = errorMessage
+                    cst_account_detail.background =
+                        ContextCompat.getDrawable(this@MainActivity, R.drawable.result_fail_background)
                     cst_account_detail.visibility = View.VISIBLE
                 }
 
                 override fun onApiSuccess(accountResponse: AccountResponse?) {
                     for (balance in accountResponse!!.balances) {
+                        pb_two.visibility = View.GONE
                         tv_account_detail.text =
                             String.format("Type: %s, Balance: %s", balance.assetType, balance.balance)
+                        cst_account_detail.background =
+                            ContextCompat.getDrawable(this@MainActivity, R.drawable.result_success_background)
                         cst_account_detail.visibility = View.VISIBLE
                     }
                 }
